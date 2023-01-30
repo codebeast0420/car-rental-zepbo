b94322287dd7c17f0d00a8d0bfbde68f3256b896
John <codebeast0420@gmail.com>
2023-01-30 08:51:33 +0200

    navbar footer car comonent

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Carbook - Free Bootstrap 4 Template by Colorlib</title>
+        <title>ZepBo</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet" />
@@ -31,26 +31,7 @@ export default function Home() {
         <link rel="stylesheet" href="css/style.css" />
       </Head>
       <main>
-        <nav className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-          <div className="container">
-            <a className="navbar-brand" href="index.html">Car<span>Book</span></a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="oi oi-menu"></span> Menu
-            </button>
-
-            <div className="collapse navbar-collapse" id="ftco-nav">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item active"><a href="index.html" className="nav-link">Home</a></li>
-                <li className="nav-item"><a href="about.html" className="nav-link">About</a></li>
-                <li className="nav-item"><a href="services.html" className="nav-link">Services</a></li>
-                <li className="nav-item"><a href="pricing.html" className="nav-link">Pricing</a></li>
-                <li className="nav-item"><a href="car.html" className="nav-link">Cars</a></li>
-                <li className="nav-item"><a href="blog.html" className="nav-link">Blog</a></li>
-                <li className="nav-item"><a href="contact.html" className="nav-link">Contact</a></li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        <Navbar />
 
         <div className="hero-wrap ftco-degree-bg" style={{ backgroundImage: "url('images/bg_1.jpg')" }}>
           <div className="overlay"></div>
@@ -473,59 +454,7 @@ export default function Home() {
           </div>
         </section>
 
-        <footer className="ftco-footer ftco-bg-dark ftco-section">
-          <div className="container">
-            <div className="row mb-5">
-              <div className="col-md">
-                <div className="ftco-footer-widget mb-4">
-                  <h2 className="ftco-heading-2"><a href="#" className="logo">Car<span>book</span></a></h2>
-                  <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                  <ul className="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-                    <li className=""><a href="#"><span className="icon-twitter"></span></a></li>
-                    <li className=""><a href="#"><span className="icon-facebook"></span></a></li>
-                    <li className=""><a href="#"><span className="icon-instagram"></span></a></li>
-                  </ul>
-                </div>
-              </div>
-              <div className="col-md">
-                <div className="ftco-footer-widget mb-4 ml-md-5">
-                  <h2 className="ftco-heading-2">Information</h2>
-                  <ul className="list-unstyled">
-                    <li><a href="#" className="py-2 d-block">About</a></li>
-                    <li><a href="#" className="py-2 d-block">Services</a></li>
-                    <li><a href="#" className="py-2 d-block">Term and Conditions</a></li>
-                    <li><a href="#" className="py-2 d-block">Best Price Guarantee</a></li>
-                    <li><a href="#" className="py-2 d-block">Privacy &amp; Cookies Policy</a></li>
-                  </ul>
-                </div>
-              </div>
-              <div className="col-md">
-                <div className="ftco-footer-widget mb-4">
-                  <h2 className="ftco-heading-2">Customer Support</h2>
-                  <ul className="list-unstyled">
-                    <li><a href="#" className="py-2 d-block">FAQ</a></li>
-                    <li><a href="#" className="py-2 d-block">Payment Option</a></li>
-                    <li><a href="#" className="py-2 d-block">Booking Tips</a></li>
-                    <li><a href="#" className="py-2 d-block">How it works</a></li>
-                    <li><a href="#" className="py-2 d-block">Contact Us</a></li>
-                  </ul>
-                </div>
-              </div>
-              <div className="col-md">
-                <div className="ftco-footer-widget mb-4">
-                  <h2 className="ftco-heading-2">Have a Questions?</h2>
-                  <div className="block-23 mb-3">
-                    <ul>
-                      <li><span className="icon icon-map-marker"></span><span className="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-                      <li><a href="#"><span className="icon icon-phone"></span><span className="text">+2 392 3929 210</span></a></li>
-                      <li><a href="#"><span className="icon icon-envelope"></span><span className="text">info@yourdomain.com</span></a></li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </footer>
+        <Footer />
       </main>
     </>
   )
