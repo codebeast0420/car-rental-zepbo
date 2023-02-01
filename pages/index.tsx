64c8d5f522c626fb0bccd64a13d3078b206bed9c
John <codebeast0420@gmail.com>
2023-02-01 11:36:07 +0200

    build test zp

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     <>
       <Head>
         <meta charSet="utf-8" />
-        <title>ZepBo</title>
+        <title>ZepBo!</title>
         <meta content="width=device-width, initial-scale=1.0" name="viewport" />
         <meta content="Free HTML Templates" name="keywords" />
         <meta content="Free HTML Templates" name="description" />
