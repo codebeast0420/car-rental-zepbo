dde446c0acd5704b883a1306483d8d1c52c10a33
John <codebeast0420@gmail.com>
2023-02-07 15:49:19 +0200

    add sort bar to inventory page

--- src/components/global/mainTheme.tsx
+++ src/components/global/mainTheme.tsx
@@ -9,7 +9,7 @@ const MainTheme: FC<HasChildren> = (porps) => {
 				<title>ZepBo</title>
 				<link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap"
 					rel="stylesheet" />
-				<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald"></link>
+				<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald" />
 				<link rel="stylesheet" href="css/ionicons.min.css" />
 				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 				<link rel="stylesheet" href="css/icomoon.css" />
--- src/components/inventory/mainListBox.tsx
+++ src/components/inventory/mainListBox.tsx
@@ -6,6 +6,33 @@ const MainListBox: FC<HasChildren> = (props) => {
 	return (
 		<section className="ftco-cart  col-md-8">
 			<div className="container">
+				<div className="sort-bar d-flex align-items-center justify-content-around mb-3" style={{ borderRadius: '0.5rem' }}>
+					<p className="m-0 text-light">Showing 1-24 of 4664</p>
+					<div className="d-flex align-items-center">
+						<p className="m-0 text-light">Show:</p>
+						<select className="form-control select-num">
+							<option>24</option>
+							<option>24</option>
+							<option>24</option>
+						</select>
+					</div>
+					<select className="form-control select-sort">
+						<option>Distance(Closest)</option>
+						<option>Year(Newest First)</option>
+						<option>Year(Oldest First)</option>
+						<option>Length(Longest First)</option>
+						<option>Length(Shortest First)</option>
+						<option>Weight(Heaviest First)</option>
+						<option>Weight(Lightest First)</option>
+						<option>Price(Highest First)</option>
+						<option>Price(Lowest First)</option>
+						<option>Location(A to Z)</option>
+						<option>Location(Z to A)</option>
+					</select>
+					{/* <a style={{ cursor: 'pointer' }}><span>&#xf08a;</span><p>(0)</p></a>
+					<a style={{ cursor: 'pointer' }}></a>
+					<a style={{ cursor: 'pointer' }}></a> */}
+				</div>
 				<div className="row">
 					<div className="col-sm-12">
 						<div className="car-list">
