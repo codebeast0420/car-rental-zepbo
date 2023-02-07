80e68264db3639ffcdb0c7a4099af791d691190b
John <codebeast0420@gmail.com>
2023-02-07 07:59:11 +0200

    build error fix for filter box

--- src/components/global/mainTheme.tsx
+++ src/components/global/mainTheme.tsx
@@ -14,7 +14,6 @@ const MainTheme: FC<HasChildren> = (porps) => {
 				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 				<link rel="stylesheet" href="css/icomoon.css" />
 				<link rel="stylesheet" href="css/style.css" />
-				<script src='https://kit.fontawesome.com/a076d05399.js' crossOrigin='anonymous'></script>
 			</Head>
 			<div>
 				{porps.children}
